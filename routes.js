4feed2be166f7ed16f3b44594095ab55f5a38109
Glitch (freecodecamp-boilerplate-project-exercisetracker-2) <none>
2018-10-11 04:35:57 +0000

    📟💦 Checkpoint ./routes.js:1953958/1204

--- routes.js
+++ routes.js
@@ -1,9 +1,28 @@
 var routes = require('express')();
 var bodyParser = require('body-parser');
+var User = require('./models/User');
+var Exercise = require('./models/Exercise');
 
 routes.post('/exercise/new-user', (req, res) => {
-  console.log('req: ', req);
-  res.json({message: 'Req received!'});
+  User.find({ username: req.body.username }, (err, user) => {
+    if(err) throw err;
+    if(user) {
+      return res.json(user);
+    }else{
+      let newUser = new User({username: req.body.username});
+      newUser.save((err, uzer) => {
+        if(err) throw err;
+        return res.json(uzer);
+      });    
+    }
+  });
+});
+
+routes.get('/exercise/users', (req, res) => {
+  User.find((err, users) => {
+    if(err) throw err;
+    if(users) return res.json(users);
+  });
 });
 
 module.exports = routes;
