4f6fb3ddb3e239ce19cf3d16cd3b15f2b24f7cf0
Glitch (freecodecamp-boilerplate-project-exercisetracker-2) <none>
2018-10-11 05:40:59 +0000

    🍑🦀 Checkpoint ./routes.js:1953958/379

--- routes.js
+++ routes.js
@@ -3,9 +3,11 @@ var bodyParser = require('body-parser');
 var User = require('./models/User');
 var Exercise = require('./models/Exercise');
 
+
+//Create new User
 routes.post('/exercise/new-user', (req, res) => {
   console.log(req.body.username);
-  User.find({ username: req.body.username }, (err, user) => {
+  User.findOne({ username: req.body.username }, (err, user) => {
     if(err) throw err;
     if(user) {
       console.log('User found');
@@ -21,11 +23,24 @@ routes.post('/exercise/new-user', (req, res) => {
   });
 });
 
+//GET all users
 routes.get('/exercise/users', (req, res) => {
   console.log(req);
   User.find((err, users) => {
     if(err) throw err;
     if(users) return res.json(users);
+  });
+});
+
+routes.post('/exercise/add', (req, res) => {
+  User.findOne({ _id: req.body.userId }, (err, user) => {
+    if(err) throw err;
+    if(!user) {
+      return es.json({ message: 'No user found, please register first.' });
+    }
+    let exercise = new Exercise({
+      description: req.bo
+    })
   });
 });
 
